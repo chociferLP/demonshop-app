78ccf8d22cbfb9bd4cd8279b72686fbaf6aa31d6
chociferLP <chociferms@gmail.com>
2024-12-16 16:07:07 +0330

    ahah

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -118,7 +118,7 @@
                             @endguest
                             <form action="{{ route('cart.button') }}" method="POST">
                                 @csrf
-                                @if ($shoppingcart->user_id != $userId)
+                                @if ($shoppingcart != $userId)
                                     <button type="submit" class="btn btn-sucsses">افزودن سبد خرید</button>
                                 @endif
                             </form>

5ada6e46bb53af020ba52c9c1478efe9b3c53928
chociferLP <chociferms@gmail.com>
2024-12-19 17:11:02 +0330

    sdsdffsdfsdf

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,10 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->nullable()->after('email');
             $table->string('password');
+            $table->string('profile')->default('pro.jpg');
+            $table->string('role')->default('user');
             $table->rememberToken();
             $table->timestamps();
         });

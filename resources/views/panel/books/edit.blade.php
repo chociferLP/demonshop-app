8873841acd70a83088d01fefe5b2adaa9e8b8ccb
chociferLP <chociferms@gmail.com>
2024-12-20 16:40:23 +0330

    shit 2

--- resources/views/panel/books/edit.blade.php
+++ resources/views/panel/books/edit.blade.php
@@ -20,8 +20,8 @@
             <br>
             <div class="mb-3">
                 <label style="color: black" for="name" class="form-label">نام محصول :</label>
-                <input style="background-color: rgb(255, 243, 80)" type="text" name="name" id="name" class="form-control"
-                    value="{{ old('name', $product->name) }}" required>
+                <input style="background-color: rgb(255, 243, 80)" type="text" name="name" id="name"
+                    class="form-control" value="{{ old('name', $product->name) }}" required>
             </div>
 
             <div class="mb-3">
@@ -30,13 +30,22 @@
             </div>
             <div class="mb-3">
                 <label style="color: black" for="price" class="form-label">قیمت :</label>
-                <input style="background-color: rgb(255, 243, 80)" type="number" name="price" id="price" class="form-control"
-                    value="{{ old('price', $product->price) }}" step="0.01" required>
+                <input style="background-color: rgb(255, 243, 80)" type="number" name="price" id="price"
+                    class="form-control" value="{{ old('price', $product->price) }}" step="0.01" required>
             </div>
             <div class="mb-3">
                 <label style="color: black" for="profile_pic" class="form-label">تصویر محصول :</label>
-                <input style="background-color: rgb(255, 243, 80)" type="file" name="profile_pic" id="profile_pic" class="form-control"
-                    value="{{ old('profile_pic', $product->profile_pic) }}" required>
+                <input style="background-color: rgb(255, 243, 80)" type="file" name="profile_pic" id="profile_pic"
+                    class="form-control" value="{{ old('profile_pic', $product->profile_pic) }}" required>
+            </div>
+            <div>
+                <label style="color: black">دسته بندی مورد نظر را انتخاب کنید :</label>
+                <select name="category_id" style="background: rgb(250, 248, 146)" class="form-select form-select-lg mb-3"
+                    aria-label="Default select example" title="دسته بندی مورد نظر را انتخاب کنید">
+                    @foreach ($categories as $category)
+                        <option style="color: black" value="{{ $category->id }}">{{ $category->slug }}</option>
+                    @endforeach
+                </select>
             </div>
 
             <button type="submit" class="btn btn-primary">بروزرسانی</button>

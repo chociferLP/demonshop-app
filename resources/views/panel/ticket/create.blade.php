ce4c6b136f4a02d6e4ef20a8711457cda5947856
chociferLP <chociferms@gmail.com>
2025-01-07 10:20:46 +0330

    dcdcdc

--- resources/views/panel/ticket/create.blade.php
+++ resources/views/panel/ticket/create.blade.php
@@ -1,12 +1,23 @@
 @extends('panel.index')
 @section('content')
-<div class="form-group">
-    <label style="color: black" for="slug">موضوع تیکت</label>
-    <input style="background: rgb(243, 241, 108)" type="text" name="slug" id="slug"
-        class="form-control" required>
-</div>
-
-
-
-
+    <br><br><br><br><br><br><br>
+    <div style="margin-right: 110px" class="form-group">
+        <label style="color: black" for="title">موضوع تیکت :</label>
+        <br>
+        <input style="background-color: rgb(253, 250, 61)" type="text" name="tickettitle" id="slug" class="form-control"
+            required>
+        <br>
+        <br>
+        <label style="color: black" for="title">دسته بندی مربوطه :</label>
+        <br>
+        <select name="ticketgory" class="form-select form-select-lg mb-3" style="background-color: rgb(253, 250, 61)">
+            <option style="color: black">دسته بندی مربوطه</option>
+            @foreach ($category as $ticket)
+                <option style="color: black">
+                    {{ $ticket->name }}
+                </option>
+            @endforeach
+        </select>
+        <br>
+    </div>
 @endsection
